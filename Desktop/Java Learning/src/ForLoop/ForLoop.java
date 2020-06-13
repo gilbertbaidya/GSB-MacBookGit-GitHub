69d0e3544421363feb4df566def1ffc2eb7c8c36
gilbertBaidya <gilbert.baidya@gmail.com>
2020-06-13 04:59:50 -0700

    First Time Commit
    Test upload

---
+++ Desktop/Java Learning/src/ForLoop/ForLoop.java
@@ -0,0 +1,13 @@
+package ForLoop;
+
+public class ForLoop {
+    public static void forLoop(String[] args) {
+
+        int i;
+        for( i = 1 ; i <= 10 ; i = i + 1) {
+            System.out.println("i");
+        }
+
+    }
+}
+
